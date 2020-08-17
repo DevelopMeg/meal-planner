146c46a2b12baeced2c26484013644efda8d566e
Asia <asia.dobrowolska95@gmail.com>
2020-08-17 10:25:02 +0200

    initial commit - create main component App

---
+++ src/components/main-components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../sass/style.scss";
+import Header from "./Header";
+import RouteSections from "./RouteSections";
+import Footer from "./Footer";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Header />
+        <RouteSections />
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
