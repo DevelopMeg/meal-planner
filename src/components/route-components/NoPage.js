8d71c7e4a991bb7caad19610a4b93b1573763130
Asia <asia.dobrowolska95@gmail.com>
2020-08-29 08:36:32 +0200

    create route component - NoPage

---
+++ src/components/route-components/NoPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoPage = () => {
+  return (
+    <section className="section-nopage">
+      <h3 className="section-nopage__title">
+        Unfortunately there is no such page
+      </h3>
+    </section>
+  );
+};
+
+export default NoPage;
