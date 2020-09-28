b8db914eff8150064f4c28eaee3bba86c85ff6f2
Asia <asia.dobrowolska95@gmail.com>
2020-09-28 11:55:10 +0200

    add styles - Menu

--- src/components/subcomponents/Menu.js
+++ src/components/subcomponents/Menu.js
@@ -1,32 +1,60 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import iconOne from "../../images/menu-item-one.png";
+import iconTwo from "../../images/menu-item-two.png";
+import iconThree from "../../images/menu-item-three.png";
+import iconFour from "../../images/menu-item-four.png";
 
 const Menu = (props) => {
+  const location = useLocation().pathname;
+
   const menuTxt = [
-    { path: "/plan-meals", text: "plan meals" },
-    { path: "/shopping-list", text: "shopping list" },
-    { path: "/list-dishes", text: "list of dishes" },
-    { path: "/structure-settings", text: "meal structure settings" },
+    { path: "/plan-meals", text: "plan meals", icon: iconOne },
+    { path: "/shopping-list", text: "shopping list", icon: iconTwo },
+    { path: "/list-dishes", text: "list of dishes", icon: iconThree },
+    {
+      path: "/structure-settings",
+      text: "settings",
+      icon: iconFour,
+    },
   ];
 
   const menuItem = menuTxt.map((item, id) => {
     return (
       <li
         key={id}
-        className="menu__item"
+        className={location !== "/" ? "menu__item" : "home-menu__item"}
         onClick={props.handleClearOpenMenuItem}
       >
-        <NavLink to={item.path} className="menu__link">
-          {item.text}
+        <NavLink
+          to={item.path}
+          className={
+            location !== "/" ? "menu__link link" : "home-menu__link link"
+          }
+        >
+          {location !== "/" && (
+            <img src={item.icon} alt={item.text} className="menu__icon" />
+          )}
+          <span className={location !== "/" ? "menu__text" : "home-menu__text"}>
+            {item.text}{" "}
+            {item.text === "shopping list" ? (
+              <span className="menu__link--amount-cart">{`(${props.shoppingListLength})`}</span>
+            ) : null}
+          </span>
         </NavLink>
-        {item.text === "shopping list" ? props.shoppingListLength : null}
       </li>
     );
   });
 
   return (
-    <nav className="menu">
-      <ul className="menu__item-list">{menuItem}</ul>
+    <nav className={location !== "/" ? "menu" : "home-menu"}>
+      <ul
+        className={
+          location !== "/" ? "menu__item-list" : "home-menu__item-list"
+        }
+      >
+        {menuItem}
+      </ul>
     </nav>
   );
 };
