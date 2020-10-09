49d179510713ccbac2c6a7196d340c4e68f90c2b
Asia <asia.dobrowolska95@gmail.com>
2020-10-09 10:10:06 +0200

    Updates

---
+++ precache-manifest.2a124f0fce77b6caed8c01b70d4342c3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cf52f4f96bd64c99501809894e4555",
+    "url": "/meal-planner/index.html"
+  },
+  {
+    "revision": "0eb4057eef085d58b5c8",
+    "url": "/meal-planner/static/css/2.1444957c.chunk.css"
+  },
+  {
+    "revision": "3c728ce7aa8cafe6412a",
+    "url": "/meal-planner/static/css/main.fafec7bb.chunk.css"
+  },
+  {
+    "revision": "0eb4057eef085d58b5c8",
+    "url": "/meal-planner/static/js/2.a22dee9b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meal-planner/static/js/2.a22dee9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c728ce7aa8cafe6412a",
+    "url": "/meal-planner/static/js/main.2282f4d8.chunk.js"
+  },
+  {
+    "revision": "35cbc70de5ff749ffa7e",
+    "url": "/meal-planner/static/js/runtime-main.d9b75603.js"
+  },
+  {
+    "revision": "3b69e8798a37eb31fa95f782bba586da",
+    "url": "/meal-planner/static/media/add-edit-dish.3b69e879.jpg"
+  },
+  {
+    "revision": "5160b7c962d6fb2e406342d84a443de7",
+    "url": "/meal-planner/static/media/home-menu-item-four.5160b7c9.jpg"
+  },
+  {
+    "revision": "c529aacffc33118218812e7dbc6dfb75",
+    "url": "/meal-planner/static/media/home-menu-item-one.c529aacf.jpg"
+  },
+  {
+    "revision": "c28d6e28d788336d5cd0cfe050570ee8",
+    "url": "/meal-planner/static/media/home-menu-item-three.c28d6e28.jpg"
+  },
+  {
+    "revision": "19e1ed5ea729931c7a9f6fa6e450f3a0",
+    "url": "/meal-planner/static/media/home-menu-item-two.19e1ed5e.jpg"
+  },
+  {
+    "revision": "89aa350023bcfbb1cb9a90eb39e8524a",
+    "url": "/meal-planner/static/media/list-dishes-item-four.89aa3500.jpg"
+  },
+  {
+    "revision": "a054b4ecccffdca46fcc0d0b1bbfc630",
+    "url": "/meal-planner/static/media/list-dishes-item-one.a054b4ec.jpg"
+  },
+  {
+    "revision": "8dbc560395541aa675f2e61290e35612",
+    "url": "/meal-planner/static/media/list-dishes-item-three.8dbc5603.jpg"
+  },
+  {
+    "revision": "d9e0de4f19c1712a623509c7276ce31e",
+    "url": "/meal-planner/static/media/list-dishes-item-two.d9e0de4f.jpg"
+  },
+  {
+    "revision": "e3d3da8cce3b0187fe6f94aaa9c6f83b",
+    "url": "/meal-planner/static/media/plan-meals-item-four.e3d3da8c.jpg"
+  },
+  {
+    "revision": "0860efe567ab0be5cb35513033e72eff",
+    "url": "/meal-planner/static/media/plan-meals-item-one.0860efe5.jpg"
+  },
+  {
+    "revision": "90647b9d91c6a31ca1fdbd4c072f7046",
+    "url": "/meal-planner/static/media/plan-meals-item-three.90647b9d.jpg"
+  },
+  {
+    "revision": "2d1b9f13a4e989dd7f04ba9295f09d4c",
+    "url": "/meal-planner/static/media/plan-meals-item-two.2d1b9f13.jpg"
+  },
+  {
+    "revision": "bb9e24cc45e24a79956933cb9935d140",
+    "url": "/meal-planner/static/media/set-dish.bb9e24cc.jpg"
+  },
+  {
+    "revision": "db200720e80c3d0491d9aa442a1c87f2",
+    "url": "/meal-planner/static/media/set-dishes.db200720.jpg"
+  },
+  {
+    "revision": "a4eb0dfdb2bbf62ac38975b16125d79f",
+    "url": "/meal-planner/static/media/shopping-list-item-four.a4eb0dfd.jpg"
+  },
+  {
+    "revision": "f02ea6fff71fe113413c0aa695b111ec",
+    "url": "/meal-planner/static/media/shopping-list-item-one.f02ea6ff.jpg"
+  },
+  {
+    "revision": "dcdd260e05cbb8e17d0832bd38e32f13",
+    "url": "/meal-planner/static/media/shopping-list-item-three.dcdd260e.jpg"
+  },
+  {
+    "revision": "369587560d48686a8047789c0f11fc11",
+    "url": "/meal-planner/static/media/shopping-list-item-two.36958756.jpg"
+  }
+]);
